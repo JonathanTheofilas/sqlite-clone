df70808a55c771bf66b60644c5d2c9b9639f4ffe
JonathanT <s3942976@student.rmit.edu.au>
2025-07-19 20:38:21 +1000

    restart, meta commands added and exe file made successfully

--- main.c
+++ main.c
@@ -1,53 +1,73 @@
+#define _GNU_SOURCE
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "cli.h"
-#include "db.h"
+#include <string.h>
 
+#define COMMAND_BUFFER_SIZE 256
 
-int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    printf("Must supply a database filename.\n");
+typedef struct {
+  char buffer[COMMAND_BUFFER_SIZE];
+  ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+  input_buffer->input_length = 0;
+
+  return input_buffer;
+}
+
+void print_prompt() { printf("db > "); }
+
+void read_input(InputBuffer* input_buffer) {
+  if (fgets(input_buffer->buffer, COMMAND_BUFFER_SIZE, stdin) == NULL) {
+    printf("Error reading input\n");
     exit(EXIT_FAILURE);
   }
 
-  char* filename = argv[1];
-  Table* table = db_open(filename);
+  // Find the length of the input, excluding the newline
+  input_buffer->input_length = strlen(input_buffer->buffer);
+  if (input_buffer->buffer[input_buffer->input_length - 1] == '\n') {
+    input_buffer->input_length--;
+    input_buffer->buffer[input_buffer->input_length] = 0;
+  }
+}
 
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer);
+}
+
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    close_input_buffer(input_buffer);
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
   while (true) {
     print_prompt();
     read_input(input_buffer);
 
     if (input_buffer->buffer[0] == '.') {
-      switch (do_meta_command(input_buffer, table)) {
-        case (META_COMMAND_SUCCESS):
+      switch (do_meta_command(input_buffer)) {
+        case META_COMMAND_SUCCESS:
           continue;
-        case (META_COMMAND_UNRECOGNIZED_COMMAND):
-          printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        case META_COMMAND_UNRECOGNIZED_COMMAND:
+          printf("Unrecognized command '%s'.\n", input_buffer->buffer);
           continue;
       }
     }
 
-    Statement statement;
-    switch (prepare_statement(input_buffer, &statement)) {
-      case (PREPARE_SUCCESS):
-        break;
-      case (PREPARE_SYNTAX_ERROR):
-        printf("Syntax error. Could not parse statement.\n");
-        continue;
-      case (PREPARE_UNRECOGNIZED_STATEMENT):
-        printf("Unrecognized keyword at start of '%s'.\n",
-               input_buffer->buffer);
-        continue;
-    }
-
-    switch (execute_statement(&statement, table)) {
-      case (EXECUTE_SUCCESS):
-        printf("Executed.\n");
-        break;
-      case (EXECUTE_TABLE_FULL):
-        printf("Error: Table full.\n");
-        break;
-    }
+    printf("SQL not implemented yet.\n");
   }
 }

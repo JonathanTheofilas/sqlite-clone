7d6173c355d4f0113729a8f82c8964edc73db368
JonathanT <s3942976@student.rmit.edu.au>
2025-07-19 20:50:30 +1000

    refactored main function to support insert and select statements, defined Row and Statement structures, and implemented the prepare_statement function to parse arguments

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #define COMMAND_BUFFER_SIZE 256
 
@@ -38,6 +39,67 @@ void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer);
 }
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE + 1];
+  char email[COLUMN_EMAIL_SIZE + 1];
+} Row;
+
+typedef enum {
+  STATEMENT_INSERT,
+  STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+  StatementType type;
+  Row row_to_insert; // Only used for STATEMENT_INSERT
+} Statement;
+
+typedef enum {
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT,
+  PREPARE_SYNTAX_ERROR
+} PrepareResult;
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+                               statement->row_to_insert.username, statement->row_to_insert.email);
+    if (args_assigned < 3) {
+      return PREPARE_SYNTAX_ERROR;
+    }
+    return PREPARE_SUCCESS;
+  }
+
+  if (strcmp(input_buffer->buffer, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+typedef enum {
+  EXECUTE_SUCCESS,
+  EXECUTE_TABLE_FULL
+} ExecuteResult;
+
+ExecuteResult execute_statement(Statement* statement) {
+  switch (statement->type) {
+    case STATEMENT_INSERT:
+      printf("This is where we would do an insert.\n");
+      break;
+    case STATEMENT_SELECT:
+      printf("This is where we would do a select.\n");
+      break;
+  }
+  return EXECUTE_SUCCESS;
+}
+
 typedef enum {
   META_COMMAND_SUCCESS,
   META_COMMAND_UNRECOGNIZED_COMMAND
@@ -68,6 +130,19 @@ int main(int argc, char* argv[]) {
       }
     }
 
-    printf("SQL not implemented yet.\n");
+        Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case PREPARE_SUCCESS:
+        break;
+      case PREPARE_UNRECOGNIZED_STATEMENT:
+        printf("Unrecognized keyword at start of '%s'.\n",
+               input_buffer->buffer);
+        continue;
+      case PREPARE_SYNTAX_ERROR:
+        printf("Syntax error. Could not parse statement.\n");
+        continue;
+    }
+
+    execute_statement(&statement);
   }
 }
